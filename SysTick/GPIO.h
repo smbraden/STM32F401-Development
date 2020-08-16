1f88d03fa26a5308c6ddc12854df07ff3da676db
smbraden <58153907+smbraden@users.noreply.github.com>
2020-08-16 16:42:10 -0700

    Initial commit

---
+++ SysTick/GPIO.h
@@ -0,0 +1,18 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "stm32f401xe.h"
+
+// void configLED(uint8_t, GPIO_TypeDef*);
+// void configButton(uint8_t , GPIO_TypeDef*);
+
+#define enablePeriphClock(AHBxENR, RCC_AHB1ENR_GPIOxEN) RCC->AHBxENR |= RCC_AHB1ENR_GPIOxEN;
+
+// return 1 for high, 0 for low input
+#define InputPinStatus(GPIOx, GPIO_Pin) ((~(GPIOx->IDR) & (1 << GPIO_Pin)) == 0 ? 0 : 1)
+#define OutputHigh(GPIOx, GPIO_Pin) (GPIOx->ODR |= (1 << GPIO_Pin))
+#define OutputLow(GPIOx, GPIO_Pin) (GPIOx->ODR &= ~(1 << GPIO_Pin))
+#define Toggle(GPIOx, GPIO_Pin) (GPIOx->ODR ^= (1 << GPIO_Pin))
+
+
+#endif
---
+++ SysTick/pinDefines.h
@@ -0,0 +1,9 @@
+#ifndef PINDEFINES_H
+#define PINDEFINES_H
+
+
+#define BUTTON1	13	// PC13		Nucleo user button
+#define LED1	5	// PA5		Nucleo user LED
+
+
+#endif
